8de6ce004c7eed1a3d1ad11018bcf8be76a29456
qwikster <ocoincs@gmail.com>
2025-10-31 03:29:38 -0400

    fix alignment

---
+++ slider.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const sliders = document.querySelectorAll('.rating-slider')
+    sliders.forEach(slider => {
+        const filled = slider.querySelector('.filled');
+        const empty = slider.querySelector('.empty');
+        let starW, gap, totalW, dragging = false, startTime = 0;
+
+        fun
+    })
+})
